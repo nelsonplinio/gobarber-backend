22ad4b914930e23368a1e099fc2f8c91a1fcbf0c
Nelson Plíni <nelsonplinio94@gmail.com>
2020-04-20 19:40:53 -0300

    Abstração da logica de validação dos controllers

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,6 +1,5 @@
 import { format, isBefore, parseISO, startOfHour, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
-import * as Yup from 'yup';
 import Queue from '../../lib/Queue';
 import CancellationMail from '../jobs/CancellationMail';
 import Appointment from '../models/Appointment';
@@ -38,15 +37,6 @@ class AppointmentController {
   }
 
   async store(req, res) {
-    const schema = Yup.object().shape({
-      date: Yup.date().required(),
-      provider_id: Yup.number().required(),
-    });
-
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails' });
-    }
-
     const { provider_id, date } = req.body;
 
     /**
